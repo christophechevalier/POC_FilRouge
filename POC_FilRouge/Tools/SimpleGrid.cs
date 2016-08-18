f9dda4537db652f6c591af0eea8f49eb31fa0842
Christophe Chevalier <cchevalier@linagora.com>
2016-08-18 15:25:48 +0200

    First commit for testing module buttons
    - Test

---
+++ POC_FilRouge/Tools/SimpleGrid.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using POC_FilRouge.Tools;
+
+namespace POC_FilRouge.Tools
+{
+    public class SimpleGrid : Grid
+    {
+        private GridLengthCollection _rows;
+        public GridLengthCollection Rows
+        {
+            get { return _rows; }
+            set
+            {
+                _rows = value;
+                RowDefinitions.Clear();
+                if (_rows == null)
+                    return;
+                foreach (var length in _rows)
+                {
+                    RowDefinitions.Add(new RowDefinition { Height = length });
+                }
+            }
+        }
+
+        private GridLengthCollection _columns;
+        public GridLengthCollection Columns
+        {
+            get { return _columns; }
+            set
+            {
+                _columns = value;
+                if (_columns == null)
+                    return;
+                ColumnDefinitions.Clear();
+                foreach (var length in _columns)
+                {
+                    ColumnDefinitions.Add(new ColumnDefinition { Width = length });
+                }
+            }
+        }
+    }
+}
